29e85284abe5e6e9911d09d6010c3460c10ed29a
Finley Gibson <f.j.gibson@exeter.ac.uk>
2024-12-08 17:17:36 +0000

    added image filtering function to utilities

--- src/plottosat/utils.py
+++ src/plottosat/utils.py
@@ -1,6 +1,7 @@
 from typing import Union
 import datetime as dt
 from plottosat import logger
+from ee.imagecollection import ImageCollection
 
 
 def process_date(date: Union[str, dt.date]) -> dt.date:
@@ -15,3 +16,24 @@ def process_date(date: Union[str, dt.date]) -> dt.date:
                 f"date: {date} of invalid format, should be in format: %Y-%M-%d"
             )
             raise e
+
+
+def filter_image_collection(
+    img: ImageCollection, filter_size: int, filter_shape: str
+) -> ImageCollection:
+    """
+    Applies a median filter to an image.
+
+    Args:
+        img: The image to be filtered.
+        filter_size: Size of the filter to be applied (e.g., 3 implies a 3x3 filter).
+        filter_shape: Shape of the filter. Options include:
+                      'circle', 'square', 'cross', 'plus', 'octagon', 'diamond'.
+
+    Returns:
+        The filtered image.
+    """
+    # Apply a focal median filter with the specified size and shape
+    return img.focalMedian(filter_size, filter_shape, "pixels").copyProperties(
+        img, ["system:time_start"]
+    )
